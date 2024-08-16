fd07453156f8e5c56f4f0a111a99ed45956f4a58
rpelmegov <rpelmegov@ya.ru>
2024-08-16 20:09:07 +0300

    Update Plugin version

--- billing_example/Assets/RuStoreSDK/BillingClient/RuStoreBillingClient.cs
+++ billing_example/Assets/RuStoreSDK/BillingClient/RuStoreBillingClient.cs
@@ -8,7 +8,7 @@ namespace RuStore.BillingClient {
 
     public class RuStoreBillingClient {
 
-        public static string PluginVersion = "6.0.0";
+        public static string PluginVersion = "6.1.0";
 
         private static RuStoreBillingClient _instance;
         private static bool _isInstanceInitialized;
